594646e5829b7eb67b86201aab5678a838a8282a
vasilisium <vasilisium@gmail.com>
2021-06-15 14:08:02 +0300

    Initial updatetd

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
 
 module.exports = {
   mode: 'production',
